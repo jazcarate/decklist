4de57fa6422fa5b689e820f92f9bca111f46fe66
Joaquin Azcarate <j@florius.com.ar>
2023-07-15 00:29:25 +0200

    Search!

--- frontend/functions/admin/events/index.ts
+++ frontend/functions/admin/events/index.ts
@@ -1,5 +1,6 @@
-import { renderFull } from "../../render";
+import { renderFull, renderPartial } from "../../render";
 import listEvents from "../../../templates/admin/events/index.html";
+import eventsResult from "../../../templates/admin/events/events.html";
 import row from "../../../templates/admin/events/eventRow.html";
 
 interface Env {
@@ -10,14 +11,24 @@ interface Metadata {
     name: string,
 }
 
-export const onRequestGet: PagesFunction<Env> = async ({ env }) => {
+export const onRequestGet: PagesFunction<Env> = async ({ env, request }) => {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+
     const prefix = "events:";
     const dbEvents = await env.db.list<Metadata>({ prefix });
 
     const events = dbEvents.keys.map(event => ({
         name: event.metadata.name,
         slug: event.name.substring(prefix.length)
-    }))
+    })).filter(({ name, slug }) => {
+        if (q == null) return true;
+        return name.includes(q) || slug.includes(q);
+    })
 
-    return renderFull(listEvents, { events }, { row });
+    if (q == null) {
+        return renderFull(listEvents, { events }, { row, events: eventsResult })
+    } else {
+        return renderPartial(eventsResult, { events }, { row })
+    }
 }
