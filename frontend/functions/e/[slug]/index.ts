81dab0b1b5aae202b0d84e6d71114fe527aac560
Joaquin Azcarate <j@florius.com.ar>
2023-07-15 20:50:34 +0200

    wip Frontent (fing elipse)

--- frontend/functions/e/[slug]/index.ts
+++ frontend/functions/e/[slug]/index.ts
@@ -3,13 +3,33 @@ import list from "../../../templates/events/list.html";
 
 interface Env {
     db: KVNamespace,
-    ALL_ADMIN: boolean,
-    SIGNING_SECRET: string,
 }
 
-export const onRequestGet: PagesFunction<Env> = async (context) => {
-    const { params } = context;
-    const slug = params.slug as string;
+interface Metadata {
+    from: string;
+    name: string;
+    note?: string;
+    reviewed: boolean;
+}
 
-    return renderFull(list, { title: slug, slug });
+export const onRequestGet: PagesFunction<Env> = async ({ request, params, env }) => {
+    const url = new URL(request.url);
+    const slug = params.slug as string;
+    const q = url.searchParams.get("q");
+
+    const prefix = `event:${slug}:mail:`;
+    const mails = (await env.db.list<Metadata>({ prefix })).keys
+        .map(mail => {
+            const { from, name, note, reviewed } = mail.metadata;
+            const id = mail.name.substring(prefix.length);
+            return { from, name, note, reviewed, id };
+        })
+        .filter(({ from, name, note }) => {
+            if (q == null) return true;
+            return from?.includes(q) || name.includes(q) || note?.includes(q)
+        });
+
+    const response = renderFull(list, { title: slug, slug, mails, q });
+    response.headers.set("HX-Push-Url", url.href);
+    return response
 }
